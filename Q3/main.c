865faebe46fc497793e42a8af075393d3e04ffb6
Civan Rumet Arı <civanrumet@hotmail.com>
2024-05-21 21:49:33 +0300

    Little changes

--- Q3/main.c
+++ Q3/main.c
@@ -1,44 +1,45 @@
 #include "Q3.h"
 
-int main() {
-    
+int main()
+{
     //Velocity calculations
-    printf("Velocity Calculations:\n");
+    printf("|-----------Velocity Calculations-----------|\n");
+
     for (int i = 0; i < n; i++) 
 	{
 		printf("For point t=%.2f results:\n", t[i]);
 
         if (i < n-2)
-            printf("3 Points Forward difference at t = %.2f: %.2f\n", t[i], forward_difference_3points(h, x, i));
+            printf("3 Points Forward difference at t=%.2f, v=%.2f\n", t[i], forward_difference_3points(h, x, i));
         if (i >1)
-            printf("3 Points Backward difference at t = %.2f: %.2f\n", t[i], backward_difference_3points(h, x, i));
+            printf("3 Points Backward difference at t=%.2f, v=%.2f\n", t[i], backward_difference_3points(h, x, i));
         if (i < n-1 && i>0)
-            printf("3 Points Central difference at t = %.2f: %.2f\n", t[i], central_difference_3points(h, x, i));
+            printf("3 Points Central difference at t=%.2f, v=%.2f\n", t[i], central_difference_3points(h, x, i));
 		if (i < n-4)
-            printf("5 Points Forward difference at t = %.2f: %.2f\n", t[i], forward_difference_5points(h, x, i));
+            printf("5 Points Forward difference at t=%.2f, v=%.2f\n", t[i], forward_difference_5points(h, x, i));
         if (i < n-2 && i>1)
-            printf("5 Points Central difference at t = %.2f: %.2f\n", t[i], central_difference_5points(h, x, i));
+            printf("5 Points Central difference at t=%.2f, v=%.2f\n", t[i], central_difference_5points(h, x, i));
 		
 		printf("\n");
     }
-
     //Acceleration calculations
-    printf("\nAcceleration Calculations :\n");
+    printf("\n|-----------Acceleration Calculations-----------|\n");
+
     for (int i = 0; i < n; i++) 
     {
 		printf("For point t=%.2f results:\n", t[i]);
 
         if (i < n-2)
-            printf("3 Points Forward Acceleration at t = %.2f: %.2f\n", t[i], forward_acceleration_3points(h, x, i));
+            printf("3 Points Forward Acceleration at t=%.2f, a=%.2f\n", t[i], forward_acceleration_3points(h, x, i));
         if (i >1)
-            printf("3 Points Backward Acceleration at t = %.2f: %.2f\n", t[i], backward_acceleration_3points(h, x, i));
+            printf("3 Points Backward Acceleration at t=%.2f, a=%.2f\n", t[i], backward_acceleration_3points(h, x, i));
         if (i < n-1 && i>0)
-            printf("3 Points Central Acceleration at t = %.2f: %.2f\n", t[i], central_acceleration_3points(h, x, i));
+            printf("3 Points Central Acceleration at t=%.2f, a=%.2f\n", t[i], central_acceleration_3points(h, x, i));
 		if (i < n-2 && i>1)
-            printf("5 Points Central Acceleration at t = %.2f: %.2f\n", t[i], central_acceleration_5points(h, x, i));
+            printf("5 Points Central Acceleration at t=%.2f, a=%.2f\n", t[i], central_acceleration_5points(h, x, i));
 
 		printf("\n");
     }
-
+	getchar();
     return 0;
 }
